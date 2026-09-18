0e85fc549cd285bf40167612ccd8ee37e6bf7642
DevMitchelle <devmitchellemumbua@gmail.com>
2026-09-18 09:15:42 +0300

    Updated student grade calculator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gradeForm = document.getElementById("GradeForm");
+const gradeForm = document.getElementById("gradeForm");
 const result = document.getElementById("result");
 
 const getGrade = (average) => {
@@ -36,11 +36,32 @@ gradeForm.addEventListener("submit",(event)=>{
         const database = document.getElementById("database").value;
 
         const marks = [programming, mathematics, networking, database];
+        for(const mark of marks) {
+            if(mark<0 || mark >100 || isNaN(mark)){
+                throw new Error("Marks must be between 0 and 100");
+            }
+        }
+
         const total = calculateTotal(marks);
         const average = total / marks.length;
-        const grade = getGrade(average);  
+        const grade = getGrade(average)
+
+        result.innerHTML = `
+        <h2>Student  Results</h2>
+        <p><strong>Name: <strong> ${studentName}</p>
+        <p><strong>Programming: <strong> ${programming}</p>
+        <p><strong>Mathematics: <strong> ${mathematics}</p> 
+        <p><strong>Networking: <strong> ${networking}</p>
+        <p><strong>Database: <strong> ${database}</p>
+        <p><strong>Total Marks: <strong> ${total}</p>
+        <p><strong>Average Marks: <strong> ${average}</p>
+        <p><strong>Grade: <strong> ${grade}</p>
+        `;
+         console.log(`Results for ${studentName}`);
+         console.log( `Total: ${total}, Average: ${average}, Grade: ${grade}`);
     }
     catch(error){
+        result.innerHTML=`<p style="color:red;">Error: ${error.message}</p>`;
         console.error(error);
     }  
 
